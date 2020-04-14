d1f8de1ed00d51d9e7f481b0c5e36c83ccd365c0
aron <aronphong@gmail.com>
2020-04-14 05:47:23 -0700

    Updates

---
+++ precache-manifest.fc5bcc921609f5d7020ff66d302b7e23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23e1bef1f8ef0852c870c7053d2e2db6",
+    "url": "/React-battleship/index.html"
+  },
+  {
+    "revision": "c3f7572a672629e7a7cf",
+    "url": "/React-battleship/static/css/main.fd0f38cb.chunk.css"
+  },
+  {
+    "revision": "4500a98d8f68bc2a307e",
+    "url": "/React-battleship/static/js/2.eb1cd5e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-battleship/static/js/2.eb1cd5e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3f7572a672629e7a7cf",
+    "url": "/React-battleship/static/js/main.57510437.chunk.js"
+  },
+  {
+    "revision": "8866d8a4d04c2d14646e",
+    "url": "/React-battleship/static/js/runtime-main.1e23dfc4.js"
+  }
+]);
